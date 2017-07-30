7c8f6e2192ba3c9261715b2e6e89746de489b631
Julian Rogmans <rogmansj@gmail.com>
2017-07-30 16:42:08 -0700

    Add home page for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get "/farms", "pages#farms"
+  get "/blog", "pages#blog"
+  get "/faq", "pages#faq"
 end
